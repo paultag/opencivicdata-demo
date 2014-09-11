3ed96ffceda28062117b45f174dd26394841d6b3
Paul Tagliamonte <tag@pault.ag>
2014-09-11 18:19:57 -0400

    Update for lat/lon

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from opencivicdata.api import OCDAPI
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+api = OCDAPI(host="http://10.42.2.102")
 
 @app.route("/")
 def index():
@@ -10,6 +11,8 @@ def index():
 
 @app.route("/lookup", methods=["POST"])
 def lookup():
+    lat, lon = [request.form[x] for x in ['lat', 'lon']]
+    legs = api.people(lat=lat, lon=lon)
     return render_template("lookup.html")
 
 
